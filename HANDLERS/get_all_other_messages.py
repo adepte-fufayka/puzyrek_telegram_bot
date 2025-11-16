2f2dfe5328a537c2bf64e63b17bf4cb19426929b
adepte fufayka <87909953+adepte-fufayka@users.noreply.github.com>
2025-11-16 18:19:50 +0300

    0

--- HANDLERS/get_all_other_messages.py
+++ HANDLERS/get_all_other_messages.py
@@ -10,14 +10,12 @@ class MessageInfo:
         self.chat_id=int(data[1])
         self.message_id=int(data[2])
         self.text="\n".join(data[4:])
-@bot.on_message(filters.chat(ADMIN_CHAT)&filters.reply)
-async def reply(_, message: Message):
-    m=MessageInfo(message.reply_to_message.text)
-    bot.fo
+
 @bot.on_message()
 async def spam(_, message:Message):
     if message.chat.id != ADMIN_CHAT:
         try:
             await bot.send_message(ADMIN_CHAT, f"{message.chat.title if message.chat.title else '#лс'}\n{message.chat.id}\n{message.id}\n{message.from_user.first_name} {message.from_user.last_name if message.from_user.last_name else ''}\n{message.text}")
         except Exception as e:
+
             print(e)
